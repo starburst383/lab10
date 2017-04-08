08063373243e2c9ab230a9e2fae071ffd1fed85e
Andrew <blizzard383@ymail.com>
2017-04-08 12:55:03 -0500

    Update Motor.java

--- Motor.java
+++ Motor.java
@@ -2,4 +2,20 @@ package lab10;
 
 public class Motor {
 
+  int distRemain = 100;
+	boolean opening;
+	
+	public void on()
+	{
+		//closed by default. 
+		opening = true;
+	}
+	public void off()
+	{
+		
+	}
+	public void reverse()
+	{
+		opening = !opening;
+	}
 }
