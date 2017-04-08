ed7e0eb5316acb2513394fed4d7c4bf4e6d84729
Andrew <blizzard383@ymail.com>
2017-04-08 14:52:30 -0500

    Add files via upload

---
+++ StateMachine.java
@@ -0,0 +1,40 @@
+package lab10;
+
+public class StateMachine {
+int [] [] stateTable = {{1,0},{3,1,1},{4,0,2},{4,0,2}};
+int currentState;
+boolean stopped;
+public StateMachine()
+{
+	currentState = 0;
+}
+
+	public int next(int event)
+	{
+		if(currentState == 0 && event == 2)
+		{
+			System.out.println("");
+		}
+		else if(currentState == 4)
+		{
+			if(event == 2)
+			{
+				
+			}
+			if(event == 3)
+			{
+				
+			}
+		}
+		else
+		{
+			currentState = stateTable[currentState][event];
+			if(currentState == 4)
+			{
+				stopped = true;
+			}
+		}
+		return currentState;
+
+	}
+}
