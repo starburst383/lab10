c032f9d83c0f86cd85f5394ee43ee45fec756d8e
Andrew <blizzard383@ymail.com>
2017-04-08 13:06:00 -0500

    Update GarageDoorSystem.java

--- GarageDoorSystem.java
+++ GarageDoorSystem.java
@@ -2,10 +2,24 @@ package lab10;
 
 public class GarageDoorSystem {
 	GarageDoor DOOR = new GarageDoor();
+	static Scanner keyboard = new Scanner(System.in);
+	boolean on = false;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-
+		String input;
+		System.out.println("type 'POWER' to turn system on");
+			input = keyboard.nextLine();
+			
+		if(input.equalsIgnoreCase("power"))
+		{
+			System.out.println("Garage door is 'on'");
+		}
+		else
+		{
+			System.out.println("power most be turned on to use commands, enter 'Power'");
+		}
 	}
 
 }
