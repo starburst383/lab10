7ee88b405e680961e7fe31a27e1ce25876052128
Andrew <blizzard383@ymail.com>
2017-04-08 12:23:17 -0500

    Add files via upload

---
+++ GarageDoor.java
@@ -0,0 +1,31 @@
+package lab10;
+
+public class GarageDoor {
+	boolean open;
+	
+	
+	public void open()
+	{
+		
+	}
+	public void close()
+	{
+		
+	}
+	public void stop()
+	{
+		
+	}
+	public void onClick()
+	{
+		
+	}
+	public void onSaftey()
+	{
+		
+	}
+	public void onLimit()
+	{
+		
+	}
+}
---
+++ GarageDoorSystem.java
@@ -0,0 +1,10 @@
+package lab10;
+
+public class GarageDoorSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
