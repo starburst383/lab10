4e5d3ebfc9815338b091c2854aa2d0c9b2a86394
Andrew <blizzard383@ymail.com>
2017-04-08 13:34:04 -0500

    Update Motor.java

--- Motor.java
+++ Motor.java
@@ -1,21 +1,69 @@
 package lab10;
 
-public class Motor {
+import java.util.Timer;
+import java.util.TimerTask;
 
-  int distRemain = 100;
+public class Motor {
+	int distRemain; 
 	boolean opening;
+	Timer timer;
+	
+	public Motor()
+	{
+		distRemain = 100;
+		timer = new Timer();
+	}
 	
 	public void on()
 	{
 		//closed by default. 
-		opening = true;
+		
+		if(opening = true)
+		{
+		timer.schedule(new FinTaskO(), distRemain);
+		}
+		else
+		{
+			timer.schedule(new FinTaskC(), distRemain);
+		}
+		
 	}
 	public void off()
 	{
-		
+		timer = null;
+		opening = !opening;
+		timer = new Timer();
+		distRemain = 50;
 	}
 	public void reverse()
 	{
+		//timer.
+		timer = null;
 		opening = !opening;
+		timer = new Timer();
+		distRemain = 50;
+		on();
+		
 	}
+	//open timer
+	class FinTaskO extends TimerTask
+	{
+		public void run()
+		{
+			System.out.println("is open!");
+			distRemain = 100;
+			opening = false;
+		}
+	}
+	//close timer
+	class FinTaskC extends TimerTask
+	{
+		public void run()
+		{
+			System.out.println("is closed!");
+			distRemain = 100;
+			opening = true;
+		}
+	}
+
 }
