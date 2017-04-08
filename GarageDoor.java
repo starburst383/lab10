f2b36d5b62bea724fbc8434f01c8bf67244048ea
Andrew <blizzard383@ymail.com>
2017-04-08 12:42:22 -0500

    Update GarageDoor.java

--- GarageDoor.java
+++ GarageDoor.java
@@ -4,16 +4,21 @@ import java.util.Timer;
 
 public class GarageDoor {
 	boolean open;
+	boolean light;
 	Timer timer;
 	
 	public GarageDoor()
 	{
+		//closed by default
 		open = false;
+		//off by default
+		light = false;
+		//light 20 second timer
 		timer = new Timer();
 	}
 	public void open()
 	{
-		
+		timer.schedule(new lightTask(), 20);
 	}
 	public void close()
 	{
@@ -34,5 +39,14 @@ public class GarageDoor {
 	public void onLimit()
 	{
 		
+	}
+	//will turn the light off after 20 seconds
+	class lightTask extends TimerTask
+	{
+		public void run()
+		{
+			System.out.println("light off");
+			light = false;
+		}
 	}
 }
